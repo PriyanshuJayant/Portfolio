44a449420c04b3c3a9363171e6b0e0a36eaf0b90
Priyanshu Jayant <priyanshujayant8@gmail.com>
2025-09-26 23:45:23 +0530

    Alpha 1.9 (Added Hover Effect in Nav Buttons)

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -20,90 +20,119 @@ function NavBar() {
 
   return (
     <>
-      {/* <motion.div
-        className="navBar"
-        initial={{ opacity: 0.01 }}
-        animate={show ? { opacity: 1 } : { opacity: 0.01 }}
-        transition={{ duration: 0.4, ease: "easeOut" }}
-        style={{ position: "fixed", top: 0, width: "100%" }}
-      > */}
-        <motion.div
+      <motion.div
         className="navBar"
         initial={{
-           y: 0,
-           opacity: 0.01
+          y: 0,
+          opacity: 0.01,
         }}
-        animate={show ? { y: 0, opacity: 1 } : { y: 0, opacity: 0 }}
+        animate={show ? { y: 0, opacity: 1 } : { y: -50, opacity: 0 }}
         transition={{
           type: "spring",
-          stiffness: 80,
-          damping: 20
+          stiffness: 100,
+          damping: 10,
         }}
         style={{ position: "fixed", top: 0, width: "100%" }}
       >
-        <GlassElement
-          width={80}
-          height={55}
-          radius={90}
-          depth={1}
-          blur={0.5}
-          chromaticAberration={5}
-          debug={false}
-          className="nav-btn"
+        <motion.div 
+          className="glass-wrapper"
+          whileHover={{ scale: 1.05 }}
+          transition={{ duration: 0.4, ease: "easeInOut" }}
         >
-          Home
-        </GlassElement>
+          <GlassElement
+            className="nav-btn"
+            width={80}
+            height={55}
+            radius={90}
+            depth={1}
+            blur={0.4}
+            chromaticAberration={5}
+            debug={false}
+          >
+            Home
+          </GlassElement>
+        </motion.div>
 
-        <div className="nav-btn-container">
-          <GlassElement
-            width={80}
-            height={55}
-            radius={90}
-            depth={4}
-            blur={1}
-            chromaticAberration={5}
-            debug={false}
-            className="nav-btn"
-          >
-            Projects
-          </GlassElement>
-          <GlassElement
-            width={80}
-            height={55}
-            radius={90}
-            depth={4}
-            blur={1}
-            chromaticAberration={5}
-            debug={false}
-            className="nav-btn"
-          >
-            Skills
-          </GlassElement>
-          <GlassElement
-            width={80}
-            height={55}
-            radius={90}
-            depth={4}
-            blur={1}
-            chromaticAberration={5}
-            debug={false}
-            className="nav-btn"
-          >
-            About
-          </GlassElement>
-        </div>
-        <GlassElement
-          width={80}
-          height={55}
-          radius={90}
-          depth={4}
-          blur={1}
-          chromaticAberration={5}
-          debug={false}
-          className="nav-btn"
+        <motion.div 
+          className="nav-btn-container"
+          whileHover={{ scale: 1.03 }}  
+          transition={{ duration: 0.4, ease: "easeInOut" }}
         >
-          Contact
-        </GlassElement>
+          <motion.div 
+            className="glass-wrapper"
+            whileHover={{ scale: 1.05 }}
+            transition={{ duration: 0.4, ease: "easeInOut" }}
+          >
+            <GlassElement
+              className="nav-btn"
+              width={80}
+              height={55}
+              radius={90}
+              depth={4}
+              blur={1}
+              chromaticAberration={5}
+              debug={false}
+            >
+              Projects
+            </GlassElement>
+          </motion.div>
+
+          <motion.div 
+            className="glass-wrapper"
+            whileHover={{ scale: 1.05 }}
+            transition={{ duration: 0.4, ease: "easeInOut" }}
+          >
+            <GlassElement
+              className="nav-btn"
+              width={80}
+              height={55}
+              radius={90}
+              depth={4}
+              blur={1}
+              chromaticAberration={5}
+              debug={false}
+            >
+              Skills
+            </GlassElement>
+          </motion.div>
+
+          <motion.div 
+            className="glass-wrapper"
+            whileHover={{ scale: 1.05 }}
+            transition={{ duration: 0.4, ease: "easeInOut" }}
+          >
+            <GlassElement
+              className="nav-btn"
+              width={80}
+              height={55}
+              radius={90}
+              depth={4}
+              blur={1}
+              chromaticAberration={5}
+              debug={false}
+            >
+              About
+            </GlassElement>
+          </motion.div>
+        </motion.div>
+
+        <motion.div className="glass-wrapper"
+          whileHover={{ scale: 1.05 }}
+          transition={{ duration: 0.4, ease: "easeInOut" }}
+        >
+          <GlassElement
+            className="nav-btn"
+            width={80}
+            height={55}
+            radius={90}
+            depth={4}
+            blur={1}
+            chromaticAberration={5}
+            debug={false}
+          >
+            Contact
+          </GlassElement>
+        </motion.div>
       </motion.div>
     </>
   );
